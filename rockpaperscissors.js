3931cf543d1e5da23dc5e32c3e2d8d143bbd5935
AJ <aj@Deans-Air.fritz.box>
2021-08-04 22:20:17 +0100

    first commit

---
+++ rockpaperscissors.js
@@ -0,0 +1,24 @@
+
+
+function computerPlay() {
+const hands = ['rock', 'paper', 'scissors'];
+var randomHand = Math.floor(Math.random()*hands.length);
+console.log(randomHand);
+}
+
+console.log(computerPlay);
+console.log("suck my wab");
+
+
+/* if player === rock && computer === scissors
+if player === rock && computer === paper
+if player === rock && computer === rock
+if player === paper && computer === scissors
+if player === paper && computer === paper
+if player === paper && computer === rock
+if player === scissors && computer === scissors
+if player === scissors && computer === paper
+if player === scissors && computer === rock
+paper > rock > scissors > paper */ 
+
+
